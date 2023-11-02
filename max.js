647401b76d227761c806feeb414cf3eb6ed73801
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-11-02 21:24:54 +0600

    one line added

--- max.js
+++ max.js
@@ -1,8 +1,21 @@
-var business = 420;
-var lader    = 340;
+var business = 720;
+var lader    = 940;
+var socib    = 550;
 if( business > lader){
+  if(business > socib){
     console.log("business is bigger");
-
-} else{
-    console.log("lader is bigger");
+  }
+  else{
+    console.log("socib  is bigger");
 }
+
+}   
+else{ 
+    if( lader > socib){
+        console.log("lader is bigger");
+    }
+    else{ 
+        console.log("socib is bigger");
+    }
+}
+
