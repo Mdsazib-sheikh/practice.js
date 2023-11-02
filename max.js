313b0d2c5c6d07b22ae349ce4628bb81068c16d4
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-11-02 23:03:44 +0600

    new line add

--- max.js
+++ max.js
@@ -1,21 +1,23 @@
-var business = 720;
-var lader    = 940;
-var socib    = 550;
-if( business > lader){
-  if(business > socib){
-    console.log("business is bigger");
-  }
-  else{
-    console.log("socib  is bigger");
-}
+var business = 120;
+var lader    = 240;
+var socib    = 350;
+var max = Math.max(business, lader,socib)
+console.log(max);
+// if( business > lader){
+//   if(business > socib){
+//     console.log("business is bigger");
+//   }
+//   else{
+//     console.log("socib  is bigger");
+// }
 
-}   
-else{ 
-    if( lader > socib){
-        console.log("lader is bigger");
-    }
-    else{ 
-        console.log("socib is bigger");
-    }
-}
+// }   
+// else{ 
+//     if( lader > socib){
+//         console.log("lader is bigger");
+//     }
+//     else{ 
+//         console.log("socib is bigger");
+//     }
+// }
 
