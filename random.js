75c8c6b26de3473cfff1c69ccaa8bb0cf4e3f396
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-11-02 20:09:07 +0600

    new line add

--- random.js
+++ random.js
@@ -1,5 +1,10 @@
 var num= 2.12347;
 var result = Math.floor(num);
-var result2 =Math.ceil(num);
-console.log(result);
-console.log(result2);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num)
+var randomNum = Math.random() *10;
+var output    = Math.round(randomNum);
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+console.log(output);
