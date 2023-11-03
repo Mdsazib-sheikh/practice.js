7085dca9ca7807563e00aa720a74a62aebb3976c
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-11-03 20:43:15 +0600

    one line add

--- arrySum.js
+++ arrySum.js
@@ -1,7 +1,19 @@
-var numbers =[12,23,24,4,67];
-var sum = 0;
+// var numbers =[12,23,24,4,67];
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++){
+//     var element = numbers[i];
+//      sum     = sum + element;
+// }
+// console.log("total   of numbers : =", sum);
+function getArrySum(numbers)
+{
+    var sum = 0;
 for (var i = 0; i < numbers.length; i++){
     var element = numbers[i];
      sum     = sum + element;
 }
-console.log("total   of numbers : =", sum);
+return sum;
+}
+var numbers =[12,23,24,4,67];
+var result = getArrySum(numbers);
+console.log("total of the numbers: =", result);
