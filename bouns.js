0e367c6c921dc85b41622d5650c2e6ad3c3fcc94
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-11-10 22:09:40 +0600

    new line add

---
+++ bouns.js
@@ -0,0 +1,21 @@
+var marks = [12,43,45,67,89,98];
+
+for(var i = 0; i<marks.length; i++){
+       var element = marks[i];
+       console.log(element);
+}
+
+console.log('outside');
+console.log('gulisthan');
+function add(number1,number2,number3){
+    var total = number1+number2+number3;
+    return total;
+}
+var result1 = add(65,67,68);
+console.log(result1);
+
+console.log('not adding this time');
+console.log('not eting right now');
+
+var result2 = add(112,1123);
+console.log(result2);
